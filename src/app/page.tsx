590d4ca8c6c08be1c12f4ab67853c63560388649
이동아 <christar99@naver.com>
2024-11-18 21:50:52 +0900

    👷ci: 배포 테스트 커밋

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,41 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <pre>
+        세상에 70억명의 호날두 팬이 있다면, 나는 그들 중 한 명일 것이다. 세상에 1억명의 호날두 팬이
+        있다면., 나 또한 그들 중 한 명일 것이다. 세상에 천만 명의 호날두 팬이 있다면, 나는 여전히
+        그들 중 한 명일 것이다. 세상에 백 명의 호날두 팬이 있다면, 나는 아직도 그들 중 한 명일
+        것이다. 세상에 한 명의 호날두 팬이 있다면, 그 사람은 아마도 나일 것이다. 세상에 단 한 명의
+        호날두 팬도 없다면, 나는 그제서야 이 세상에 없는 것이다. 갓두 : 호날두를 흔히 빨때 쓰는 말
+        킹갓두 : 호날두가 멀티골을 넣었을때 쓰는 말 호킹갓두 : 호날두를 격하게 찬양하는 말 도장두 :
+        호날두가 챔피언스리그에서 여러명문팀들을 도장깨기하듯이 박살냈을때 쓰는 말 호트트릭 :
+        호날두가 헤트트릭을 했을때 쓰는 말 빛두 : 밝게 빛나는 호날두를 가리키는 말 배려두 : 동료
+        선수가 부진에 빠졌을때 pk를 양보하는 엄청난 배려심을 가진 호날두에게 쓰는 말 제너럴두 :
+        엄청난 리더쉽을 가진 호날두를 부르는 말 스피두 : 빠른 스피드로 측면을 허물었을때 호날두를
+        가리키는 말 마틴루터킹두 : 세계평화에 앞장서는 호날두를 가리키는 말 싸이월두 : 한때 모든
+        사람들을 소통하게 만들었던 싸이월드처럼 호날두가 우리를 하나로 만들었을때 쓰는 말 부동산두 :
+        집을 잃은 소년에게 집을 선물하고 에이전트에게 섬을 선물한 호날두를 가리키는 말 신짱두 :
+        짱구의 장난은 못말리듯이 호날두의 플레이를 못말리는 수비가 나왔을때 쓰는 말 위자두 :
+        마법같은 슛을 차는 호날두를 지칭하는 말 호그와두 : 마법과 같은 드리블을 할때의 호날두를 보고
+        마법학교출신이 아닐까 의심될때 호날두를 가리키는 말 해피두 : 경기자체를 즐기는 호날두를
+        가리키는 말 천사두 : 기부와 헌혈, 선행을 밥먹듯이 하는 호날두를 가리키는 말 호니버터칩 :
+        먹어도 먹어도 질리지않는 단짠단짠처럼 봐돠봐도 질리지않는 호날두의 플레이를 가리키는 말
+        호날두, 나의 사랑. 호날두, 나의 빛. 호날두, 나의 어둠. 호날두, 나의 삶. 호날두, 나의 기쁨.
+        호날두, 나의 슬픔. 호날두, 나의 고통. 호날두, 나의 안식. 호날두, 나. 호날두 뒤에서 걷지
+        마라. 호날두는 그대를 이끌지 않을 수도 있다. 호날두 앞에서 걷지 마라. 호날두는 그대를 따르지
+        않을 수도 있다. 다만 호날두 옆에서 걸으라. 호날두의 벗이 될 수 있도록. “호날두. 내 삶의
+        빛이요, 내 생명의 불꽃. 나의 죄, 나의 영혼. 크리스티아누 호날두 내가 당신을 그리워하는 것은
+        까닭이 없는 것이 아닙니다 다른 사람들은 나의 미소만을 사랑하지만 당신은 나의 눈물도 사랑하는
+        까닭입니다 호날두 당신을 사랑합니다. 영원히 사랑하겠습니다. 당신은 사랑받기 위해 태어난
+        사람이기 때문이죠. 당신을 응원한다는건 저에겐 크나큰 자랑이에요. 언제나 그보다 더 많은걸
+        주셔서 항상 감사해요. 이미 당신은 내가 존재하는 단 하나의 이유가 되어버렸거든요.
+        호날두이기에 당신을 이해하고 호날두이기에 당신의 플레이에 열광하고 호날두이기에 우리는
+        당신을 사랑합니다. 존재만으로도 우리를 기쁘게 해준 그사람. 영원히 잊지 못할 그 이름 축신두
+        오늘도 당신의 승리의 날이 될거라고 믿습니다. 호기견이라는 단어는 호날두 + 유기견이라는
+        뜻이다. 마치 유기견이 주인을 기다리듯이, 축구를 즐기는 사람이라면 무릇 축구 그 자체인
+        호날두를 그저 애원하고 갈구할 수밖에 없게 되는 축구팬들의 모습을 잘 대변한 단어라고
+        생각한다. 어쩌면 축구라는 스포츠를 향유하는 우리 모두는 사실 호기견이 아닐까?
+      </pre>
     </div>
   );
 }
